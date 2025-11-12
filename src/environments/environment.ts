c0185c5cfc93c263448de1feb91db6dcad62d4d5
srinageshkg <srinageshkg@gmail.com>
2025-11-12 16:48:52 -0600

    wip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDKSheQvwar3OBtP7_PB1FV_rB_jGnBaTo",
-    authDomain: "classic-country-homes.firebaseapp.com",
-    projectId: "classic-country-homes",
-    storageBucket: "classic-country-homes.firebasestorage.app",
-    messagingSenderId: "304725886423",
-    appId: "1:304725886423:web:f9e21767d84a226ad33792"
+
   }
 };
 
